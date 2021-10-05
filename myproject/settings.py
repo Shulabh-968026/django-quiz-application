176fe3c303c71f9e2f2d3b5afe5686dd525b3422
Shulabh-968026 <58615131+Shulabh-968026@users.noreply.github.com>
2021-10-05 15:28:25 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xt@iqsx_+&1=t=$$z*u@qp8eaz$&*^6)bp#57i9gu-m)iohlgj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoquiz-app.herokuapp.com']
 
 
 # Application definition
